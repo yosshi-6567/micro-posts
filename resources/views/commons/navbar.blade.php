9a58a5d2221cab95347b88324ca20ac9f40c9930
yoshinobu.fukao <yoshinobu6567@gmail.com>
2019-09-16 11:51:54 +0000

    Procfile

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,7 +13,7 @@
                     <li class="nav-item">{!! link_to_route('users.index', 'Users', [], ['class' => 'nav-link']) !!}</li>
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
-                         <ul class="dropdown-menu dropdown-menu-right">
+                        <ul class="dropdown-menu dropdown-menu-right">
                                 <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['id' => Auth::id()]) !!}</li>
                                 <li class="dropdown-divider"></li>
                                 <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
